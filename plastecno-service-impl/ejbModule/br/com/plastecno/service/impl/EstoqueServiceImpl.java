01ebb259e15122dcc6df06395a2bbc488e2d831f
vinicius <viniciussf@hotmail.com>
2015-01-31 00:19:37 -0200

    Implementacao de rotina para calcular o valor medio dos itens do estoque.

--- plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/EstoqueServiceImpl.java
+++ plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/EstoqueServiceImpl.java
@@ -1,12 +1,17 @@
 package br.com.plastecno.service.impl;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import br.com.plastecno.service.EstoqueService;
 import br.com.plastecno.service.PedidoService;
+import br.com.plastecno.service.constante.FormaMaterial;
 import br.com.plastecno.service.constante.SituacaoPedido;
 import br.com.plastecno.service.entity.ItemEstoque;
 import br.com.plastecno.service.entity.ItemPedido;
@@ -17,7 +22,24 @@ public class EstoqueServiceImpl implements EstoqueService {
 	@EJB
 	private PedidoService pedidoService;
 
-	private ItemEstoque geraritemEstoque(ItemPedido itemPedido) {
+	@PersistenceContext(name = "plastecno")
+	private EntityManager entityManager;
+
+	private void calcularValorMedio(ItemEstoque itemEstoque, ItemPedido itemPedido) {
+		if (itemEstoque.getId() == null) {
+			itemEstoque.setPrecoMedio(itemPedido.getPrecoUnidade());
+		} else {
+			final double valorEstoque = itemEstoque.getQuantidade() * itemEstoque.getPrecoMedio();
+			final double valorItem = itemPedido.getQuantidade() * itemPedido.getPrecoUnidade();
+			final double quantidadeTotal = itemEstoque.getQuantidade() + itemPedido.getQuantidade();
+			final double precoMedio = (valorEstoque + valorItem) / quantidadeTotal;
+
+			itemEstoque.setPrecoMedio(precoMedio);
+			itemEstoque.setQuantidade((int) quantidadeTotal);
+		}
+	}
+
+	private ItemEstoque gerarItemEstoque(ItemPedido itemPedido) {
 
 		ItemEstoque itemEstoque = new ItemEstoque();
 
@@ -41,7 +63,65 @@ public class EstoqueServiceImpl implements EstoqueService {
 			pedido.setSituacaoPedido(SituacaoPedido.COMPRA_RECEBIDA);
 		}
 		itemPedido.setRecebido(true);
-		ItemEstoque itemEstoque = geraritemEstoque(itemPedido);
 
+		Integer idMaterial = itemPedido.getMaterial().getId();
+		FormaMaterial formaMaterial = itemPedido.getFormaMaterial();
+		Double medidaExt = itemPedido.getMedidaExterna();
+		Double medidaInt = itemPedido.getMedidaInterna();
+		Double comp = itemPedido.getComprimento();
+
+		// Verificando se existe item equivalente no estoque, caso nao exista vamos
+		// criar um novo.
+		ItemEstoque itemEstoque = pesquisarItemEstoque(idMaterial, formaMaterial, medidaExt, medidaInt, comp);
+		boolean isNovo = itemEstoque == null;
+		if (isNovo) {
+			itemEstoque = gerarItemEstoque(itemPedido);
+		}
+		calcularValorMedio(itemEstoque, itemPedido);
+
+		if (isNovo) {
+			entityManager.persist(itemEstoque);
+		} else {
+			entityManager.merge(itemEstoque);
+		}
+	}
+
+	private boolean isEquivalente(Double val1, Double val2) {
+		if (val1 == null && val2 == null) {
+			return true;
+		}
+
+		if ((val1 != null && val2 == null) || (val1 == null && val2 != null)) {
+			return false;
+		}
+		final double tolerancia = 0.001;
+		return Math.abs(1 - val1 / val2) <= tolerancia;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<ItemEstoque> pesquisarItemEstoque(Integer idMaterial, FormaMaterial formaMaterial) {
+		return entityManager
+				.createQuery(
+						"select i from ItemEstoque i where i.material.id = :idMaterial and i.formaMaterial = :formaMaterial")
+				.setParameter("idMaterial", idMaterial).setParameter("formaMaterial", formaMaterial).getResultList();
+	}
+
+	public ItemEstoque pesquisarItemEstoque(Integer idMaterial, FormaMaterial formaMaterial, Double medidaExterna,
+			Double medidaInterna, Double comprimento) {
+		List<ItemEstoque> listItem = pesquisarItemEstoque(idMaterial, formaMaterial);
+		for (ItemEstoque item : listItem) {
+			if (!isEquivalente(medidaExterna, item.getMedidaExterna())) {
+				continue;
+			}
+			if (!isEquivalente(medidaInterna, item.getMedidaInterna())) {
+				continue;
+			}
+			if (!isEquivalente(comprimento, item.getComprimento())) {
+				continue;
+			}
+			return item;
+		}
+		return null;
 	}
 }
--- plastecno-service/src/br/com/plastecno/service/EstoqueService.java
+++ plastecno-service/src/br/com/plastecno/service/EstoqueService.java
@@ -1,8 +1,15 @@
 package br.com.plastecno.service;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import br.com.plastecno.service.constante.FormaMaterial;
+import br.com.plastecno.service.entity.ItemEstoque;
 
 @Local
 public interface EstoqueService {
 	void inserirItemEstoque(Integer idItemPedido);
+
+	List<ItemEstoque> pesquisarItemEstoque(Integer idMaterial, FormaMaterial formaMaterial);
 }

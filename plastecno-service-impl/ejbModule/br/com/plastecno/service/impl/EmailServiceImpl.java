a0f4de354a9f2582cce016c1c870c36a0195413a
vinicius <viniciussf@hotmail.com>
2015-02-05 22:32:13 -0200

    Remocao do envio de email que estava comentado para teste.

--- plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/EmailServiceImpl.java
+++ plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/EmailServiceImpl.java
@@ -26,52 +26,50 @@ public class EmailServiceImpl implements EmailService {
 
 	@EJB
 	private ConfiguracaoSistemaService configuracaoSistemaService;
-	
+
 	@EJB
 	private UsuarioService usuarioService;
-	
+
 	@EJB
 	private AutenticacaoService autenticacaoService;
-	
+
 	public void enviar(MensagemEmail mensagemEmail) throws NotificacaoException {
-		
+
 		try {
 			final String REMETENTE = mensagemEmail.getRemetente();
 			final String DESTINATARIO = mensagemEmail.getDestinatario();
-			
-			final String SENHA = this.autenticacaoService.decriptografar(
-					this.usuarioService.pesquisarSenhaByEmail(REMETENTE));
-			
-			
+
+			final String SENHA = this.autenticacaoService
+					.decriptografar(this.usuarioService.pesquisarSenhaByEmail(REMETENTE));
+
 			MultiPartEmail email = new HtmlEmail();
-			email.setHostName(
-					this.configuracaoSistemaService.pesquisar(ParametroConfiguracaoSistema.NOME_SERVIDOR_SMTP));
-			email.setSmtpPort(Integer.parseInt(
-					this.configuracaoSistemaService.pesquisar(ParametroConfiguracaoSistema.PORTA_SERVIDOR_SMTP)));
+			email.setHostName(this.configuracaoSistemaService.pesquisar(ParametroConfiguracaoSistema.NOME_SERVIDOR_SMTP));
+			email.setSmtpPort(Integer.parseInt(this.configuracaoSistemaService
+					.pesquisar(ParametroConfiguracaoSistema.PORTA_SERVIDOR_SMTP)));
 			email.setAuthenticator(new DefaultAuthenticator(REMETENTE, SENHA));
-			email.setSSLOnConnect(Boolean.valueOf(
-					this.configuracaoSistemaService.pesquisar(ParametroConfiguracaoSistema.SSL_HABILITADO_PARA_SMTP)));		
+			email.setSSLOnConnect(Boolean.valueOf(this.configuracaoSistemaService
+					.pesquisar(ParametroConfiguracaoSistema.SSL_HABILITADO_PARA_SMTP)));
 			email.setSubject(mensagemEmail.getTitulo());
-			
+
 			if (StringUtils.isEmpty(REMETENTE)) {
 				throw new NotificacaoException("Endereco de email do remetente eh obrigatorio");
 			}
-			
+
 			if (StringUtils.isEmpty(DESTINATARIO)) {
 				throw new NotificacaoException("Endereco de email para envio eh obrigatorio");
 			}
-			
+
 			if (StringUtils.isEmpty(mensagemEmail.getConteudo())) {
 				throw new NotificacaoException("Conteudo do email eh obrigatorio");
 			}
-			
+
 			email.setFrom(REMETENTE);
 			email.addTo(DESTINATARIO);
 			email.setMsg(mensagemEmail.getConteudo());
-			
+
 			this.gerarAnexo(mensagemEmail, email);
-			
-			//email.send();
+
+			email.send();
 		} catch (Exception e) {
 			StringBuilder mensagem = new StringBuilder();
 			mensagem.append("Falha no envio de email de ");
@@ -85,8 +83,8 @@ public class EmailServiceImpl implements EmailService {
 	private void gerarAnexo(MensagemEmail mensagemEmail, MultiPartEmail email) throws EmailException, IOException {
 		if (mensagemEmail.contemAnexo()) {
 			for (AnexoEmail anexo : mensagemEmail.getListaAnexo()) {
-				email.attach(new ByteArrayDataSource(anexo.getConteudo(), anexo.getTipoAnexo()), 
-						anexo.getNome(), anexo.getDescricao());
+				email.attach(new ByteArrayDataSource(anexo.getConteudo(), anexo.getTipoAnexo()), anexo.getNome(),
+						anexo.getDescricao());
 			}
 		}
 	}

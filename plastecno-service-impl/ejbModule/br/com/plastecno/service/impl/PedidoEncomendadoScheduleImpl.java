087634a9330254890d027dd2633c5fb4e34eb0d8
vinicius <viniciussf@hotmail.com>
2015-03-07 08:10:14 -0300

    Inclusao do agendamento de pedido encomendado

--- plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/PedidoEncomendadoScheduleImpl.java
+++ plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/PedidoEncomendadoScheduleImpl.java
@@ -37,7 +37,7 @@ public class PedidoEncomendadoScheduleImpl implements PedidoEncomendadoSchedule 
 		logger.info("Inicializando o monitoramento dos itens de pedidos encomendados aguardando reserva");
 	}
 
-	@Schedule(second = "*/60", minute = "*", hour = "*")
+	@Schedule(second = "*/300", minute = "*", hour = "*")
 	public void reservarItemPedidoEncomendadoEstoque() throws BusinessException {
 		List<Integer> listaItem = pedidoService.pesquisarIdPedidoRevendaPendenteEncomenda();
 		boolean reservado = false;

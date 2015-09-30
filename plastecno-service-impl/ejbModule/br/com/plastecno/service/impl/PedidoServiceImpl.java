e45959595a3965a6278c1982cd540f0735b4240d
vinicius <viniciussf@hotmail.com>
2015-09-30 20:28:43 -0300

    Inclusao de tratamento de ipi zero.

--- plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/PedidoServiceImpl.java
+++ plastecno-service-impl/ejbModule/br/com/plastecno/service/impl/PedidoServiceImpl.java
@@ -654,7 +654,7 @@ public class PedidoServiceImpl implements PedidoService {
 		final boolean ipiImportado = TipoApresentacaoIPI.OCASIONAL.equals(tipoApresentacaoIPI)
 				&& materialService.isMaterialImportado(itemPedido.getMaterial().getId());
 
-		if (ipiPreenchido && TipoApresentacaoIPI.NUNCA.equals(tipoApresentacaoIPI)) {
+		if (ipiPreenchido && aliquotaIPI > 0 && TipoApresentacaoIPI.NUNCA.equals(tipoApresentacaoIPI)) {
 			throw new BusinessException(
 					"Remova o valor do IPI do item pois representada escolhida não apresenta cáculo de IPI.");
 		} else if (!ipiPreenchido && (ipiObrigatorio || ipiImportado)) {

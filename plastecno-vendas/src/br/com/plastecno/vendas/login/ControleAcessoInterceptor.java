b61bd6dbf94da2a9537580e87e883798f324ab8a
vinicius <viniciussf@hotmail.com>
2017-01-09 07:45:58 -0200

    Inclusao de stacktrace no tratamento das requisicoes no controle de acesso

--- plastecno-vendas/src/br/com/plastecno/vendas/login/ControleAcessoInterceptor.java
+++ plastecno-vendas/src/br/com/plastecno/vendas/login/ControleAcessoInterceptor.java
@@ -80,11 +80,11 @@ public class ControleAcessoInterceptor implements Interceptor {
                 mensagem = "Tentativa de acesso por usuario nao autenticado ou timeoute de sessao";
             }
             mensagem += ". Possivel causa: " + e.getMessage();
-            logger.log(Level.WARNING, mensagem);
+            logger.log(Level.WARNING, mensagem, e);
         } catch (RuntimeException e) {
             logger.log(Level.SEVERE,
                     "Falha na interceptacao do metodo \"" + metodo.getMethod().getName() + "\" efetuado pelo usuario: "
-                            + usuarioInfo.getDescricaoLogin() + ". Possivel causa: " + e.getMessage());
+                            + usuarioInfo.getDescricaoLogin() + ". Possivel causa: " + e.getMessage(), e);
             throw e;
         }
     }

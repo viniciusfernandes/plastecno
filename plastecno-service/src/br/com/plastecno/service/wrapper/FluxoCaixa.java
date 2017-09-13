1f06bcbac076202be96bbcb205660990d0f40142
vinicius <viniciussf@hotmail.com>
2017-09-13 15:46:40 -0300

    Inicio de rotina para gerar os fluxos de caixa.

--- plastecno-service/src/br/com/plastecno/service/wrapper/FluxoCaixa.java
+++ plastecno-service/src/br/com/plastecno/service/wrapper/FluxoCaixa.java
@@ -1,165 +1,102 @@
 package br.com.plastecno.service.wrapper;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import br.com.plastecno.service.constante.TipoPagamento;
 import br.com.plastecno.service.exception.BusinessException;
+import br.com.plastecno.util.DateUtils;
+
+class Fluxo implements Cloneable {
+	private final int ano;
+	private final int mes;
+	private final int dia;
+	private double valPagamento;
+	private double valDuplicata;
+	private double valCredICMS;
+	private TipoPagamento tipoPagamento;
+	private double valFluxo;
+	private Calendar dtVencimento;
+
+	public Fluxo(Calendar dtVencimento, double valPagamento, TipoPagamento tipoPagamento, double valCredICMS,
+			double valDuplicata) {
+		this.dtVencimento = dtVencimento;
+		this.ano = this.dtVencimento.get(Calendar.YEAR);
+		this.mes = this.dtVencimento.get(Calendar.MONTH);
+		this.dia = this.dtVencimento.get(Calendar.DAY_OF_MONTH);
+		this.tipoPagamento = tipoPagamento;
+		this.valCredICMS = valCredICMS;
+		this.valPagamento = valPagamento;
+		this.valDuplicata = valDuplicata;
+		calcularValoFluxo();
+	}
+
+	public void adicionar(double valPagamento, double valCredICMS, double valDuplicata) {
+		this.valPagamento += valPagamento;
+		this.valCredICMS += valCredICMS;
+		this.valDuplicata += valDuplicata;
+		calcularValoFluxo();
+	}
+
+	private void calcularValoFluxo() {
+		valFluxo = (this.valDuplicata - this.valPagamento) + this.valCredICMS;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public Calendar getDtVencimento() {
+		return dtVencimento;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public TipoPagamento getTipoPagamento() {
+		return tipoPagamento;
+	}
+
+	public double getValCredICMS() {
+		return valCredICMS;
+	}
+
+	public double getValDuplicata() {
+		return valDuplicata;
+	}
+
+	public double getValFluxo() {
+		return valFluxo;
+	}
+
+	public double getValPagamento() {
+		return valPagamento;
+	}
+
+}
 
 public class FluxoCaixa {
-	private class Node {
-		private final String key;
-		private Node next;
-		private Node parent;
-		private Node previous;
-		private Node subnode;
-
-		private double value;
-
-		public Node(String key, double value) {
-			if (key == null) {
-				throw new IllegalStateException("A chave para a criacao do node nao pode ser nula.");
-			}
-
-			this.key = key;
-			this.value = value;
-		}
-
-		public void addNext(Node next) {
-			if (!this.hasParent()) {
-				throw new IllegalStateException("Tentativa de adicionar um node a um node sem pai.");
-			}
-
-			if (next != null) {
-				next.parent = this.parent;
-			}
-			addNext(this, next);
-
-		}
-
-		private void addNext(Node node, Node newNode) {
-			if (newNode == null || node == null) {
-				return;
-			}
-			if (newNode.equals(node)) {
-				node.value = newNode.value;
-				return;
-			}
-
-			if (newNode.isAfter(node)) {
-				if (!node.hasNext()) {
-					node.next = newNode;
-					newNode.previous = node;
-					newNode.next = null;
-					return;
-				}
-
-				if (node.hasNext()) {
-					if (newNode.equals(node.next)) {
-						node.next.value = newNode.value;
-						return;
-					}
-
-					if (newNode.isBefore(node.next)) {
-						newNode.next = node.next;
-						newNode.previous = node;
-
-						node.next.previous = newNode;
-						node.next = newNode;
-						return;
-					}
-
-					if (newNode.isAfter(node.next)) {
-						addNext(node.next, newNode);
-					}
-				}
-			}
-
-			if (newNode.isBefore(node)) {
-				if (!node.hasPrevious()) {
-					node.previous = newNode;
-					newNode.next = node;
-					newNode.previous = null;
-					return;
-				}
-
-				if (node.hasPrevious()) {
-					if (node.previous.equals(newNode)) {
-						node.previous.value = newNode.value;
-						return;
-					}
-
-					if (newNode.isAfter(node.previous)) {
-						newNode.previous = node.previous;
-						newNode.next = node;
-
-						node.previous.next = newNode;
-						node.previous = newNode;
-						return;
-					}
-
-					if (newNode.isBefore(node.previous)) {
-						addNext(node.previous, newNode);
-					}
-				}
-			}
-		}
-
-		public void addSubnode(Node subnode) {
-			if (subnode == null) {
-				return;
-			}
-
-			subnode.parent = this;
-
-			if (!this.hasSubnodes()) {
-				this.subnode = subnode;
-				return;
-			}
-			addNext(this.subnode, subnode);
-		}
-
-		public int compareTo(Node o) {
-			return key.compareTo(o.key);
-		}
-
-		@Override
-		public int hashCode() {
-			return key.hashCode();
-		}
-
-		public boolean hasNext() {
-			return next != null && !next.equals(previous);
-		}
-
-		public boolean hasParent() {
-			return this.parent != null;
-		}
-
-		public boolean hasPrevious() {
-			return this.previous != null;
-		}
-
-		public boolean hasSubnodes() {
-			return subnode != null;
-		}
-
-		public boolean isAfter(Node node) {
-			return this.compareTo(node) > 0;
-		}
-
-		public boolean isBefore(Node node) {
-			return this.compareTo(node) < 0;
-		}
-	}
 
 	private Date dataFinal;
 	private Date dataInicial;
 
-	private Map<Date, double[]> fluxo;
+	private List<Integer> lAno = new ArrayList<>();
+
+	private List<Integer> lMes = new ArrayList<>();
+
+	private List<Fluxo> lFluxo = new ArrayList<>();
 
 	public FluxoCaixa(Periodo periodo) {
-		fluxo = new HashMap<>();
+		super();
 		this.dataInicial = periodo.getInicio();
 		this.dataFinal = periodo.getFim();
 
@@ -168,38 +105,137 @@ public class FluxoCaixa {
 		}
 	}
 
-	private void add(Date dt, Double val, int pos) {
-		double[] valores = null;
-		if ((valores = fluxo.get(dt)) == null) {
-			valores = new double[3];
-			fluxo.put(dt, valores);
-		}
-		valores[pos] = val == null ? 0 : val;
-	}
-
-	public void addCreditoICMS(Date dtVencimento, Double valor) throws BusinessException {
-
-		if (!isDataVencimentoValida(dtVencimento)) {
-			throw new BusinessException("A data de credito de ICMS esta fora do periodo definido para o fluxo de caixa");
-		}
-		add(dtVencimento, valor, 2);
-	}
-
 	public void addDuplicata(Date dtVencimento, Double valor) throws BusinessException {
 
 		if (!isDataVencimentoValida(dtVencimento)) {
 			throw new BusinessException(
 					"A data de vencimento da duplicata esta fora do periodo definido para o fluxo de caixa");
 		}
-		add(dtVencimento, valor, 0);
+		addFluxo(new Fluxo(DateUtils.gerarCalendarioSemHorario(dtVencimento), 0, null, 0, valor));
 	}
 
-	public void addPagamento(Date dtVencimento, Double valor) throws BusinessException {
+	public void addFluxo(Fluxo fluxo) {
+		if (fluxo == null) {
+			return;
+		}
+		if (fluxo.getDtVencimento() == null) {
+			throw new IllegalStateException("A data eh necessaria para inserir o fluxo de caixa.");
+		}
+		lAno.add(fluxo.getAno());
+		lMes.add(fluxo.getMes());
+		lFluxo.add(fluxo);
+	}
+
+	public void addPagamento(Date dtVencimento, Double valor, TipoPagamento tipoPagamento, double valorCredICMS)
+			throws BusinessException {
 
 		if (!isDataVencimentoValida(dtVencimento)) {
 			throw new BusinessException("A data de pagamento esta fora do periodo definido para o fluxo de caixa");
 		}
-		add(dtVencimento, valor, 1);
+		addFluxo(new Fluxo(DateUtils.gerarCalendarioSemHorario(dtVencimento), valor, tipoPagamento, valorCredICMS, 0));
+	}
+
+	public List<Fluxo> gerarFluxoByAno() {
+		Map<Integer, Fluxo> mapAno = new HashMap<>();
+		List<Fluxo> lFluxoAno = new ArrayList<>();
+		Fluxo f = null;
+		for (Integer ano : lAno) {
+			for (Fluxo fluxo : lFluxo) {
+				if (fluxo.getAno() != ano.intValue()) {
+					continue;
+				}
+
+				if ((f = mapAno.get(ano)) == null) {
+					f = new Fluxo(fluxo.getDtVencimento(), fluxo.getValPagamento(), fluxo.getTipoPagamento(),
+							fluxo.getValCredICMS(), fluxo.getValDuplicata());
+					mapAno.put(ano, f);
+					lFluxoAno.add(f);
+				} else {
+					f.adicionar(fluxo.getValPagamento(), fluxo.getValCredICMS(), fluxo.getValDuplicata());
+				}
+			}
+
+		}
+		return lFluxoAno;
+	}
+	
+	public List<Fluxo> gerarFluxoByAnoxx() {
+		Map<Integer, Fluxo> mapAno = new HashMap<>();
+		List<Fluxo> lFluxoAno = new ArrayList<>();
+		Fluxo f = null;
+		Integer ano = null;
+			for (Fluxo fluxo : lFluxo) {
+				ano=fluxo.getAno();
+
+				if ((f = mapAno.get(ano)) == null) {
+					f = new Fluxo(fluxo.getDtVencimento(), fluxo.getValPagamento(), fluxo.getTipoPagamento(),
+							fluxo.getValCredICMS(), fluxo.getValDuplicata());
+					mapAno.put(ano, f);
+					lFluxoAno.add(f);
+				} else {
+					f.adicionar(fluxo.getValPagamento(), fluxo.getValCredICMS(), fluxo.getValDuplicata());
+				}
+
+		}
+		return lFluxoAno;
+	}
+
+	public List<Fluxo> gerarFluxoByMes() {
+		Map<Integer, Map<Integer, Fluxo>> mapMes = new HashMap<>();
+		List<Fluxo> lFluxoMes = new ArrayList<>();
+		Fluxo f = null;
+		for (Integer ano : lAno) {
+			for (Integer mes : lMes) {
+				for (Fluxo fluxo : lFluxo) {
+					if (fluxo.getAno() != ano.intValue() || fluxo.getMes() != mes.intValue()) {
+						continue;
+					}
+
+					if (!mapMes.containsKey(ano) || (f = mapMes.get(ano).get(mes)) == null) {
+						f = new Fluxo(fluxo.getDtVencimento(), fluxo.getValPagamento(), fluxo.getTipoPagamento(),
+								fluxo.getValCredICMS(), fluxo.getValDuplicata());
+						
+						Map<Integer, Fluxo> m = new HashMap<>();
+						m.put(mes, f);
+						mapMes.put(ano, m);
+
+						lFluxoMes.add(f);
+						continue;
+					}
+					f.adicionar(fluxo.getValPagamento(), fluxo.getValCredICMS(), fluxo.getValDuplicata());
+				}
+			}
+		}
+		return lFluxoMes;
+	}
+	
+	public List<Fluxo> gerarFluxoByDia() {
+		Map<Integer, Map<Integer, Map<Integer, Fluxo>>> mapDia= new HashMap<>();
+		List<Fluxo> lFluxoMes = new ArrayList<>();
+		Fluxo f = null;
+		for (Integer ano : lAno) {
+			for (Integer mes : lMes) {
+				for (Fluxo fluxo : lFluxo) {
+					if (fluxo.getAno() != ano.intValue() || fluxo.getMes() != mes.intValue()) {
+						continue;
+					}
+
+					if (!mapDia.containsKey(ano) ||!mapDia.containsKey(mes) || (f = mapDia.get(ano).get(mes)) == null) {
+						f = new Fluxo(fluxo.getDtVencimento(), fluxo.getValPagamento(), fluxo.getTipoPagamento(),
+								fluxo.getValCredICMS(), fluxo.getValDuplicata());
+						
+						Map<Integer, Fluxo> m = new HashMap<>();
+						m.put(mes, f);
+						mapMes.put(ano, m);
+
+						lFluxoMes.add(f);
+						continue;
+					}
+					f.adicionar(fluxo.getValPagamento(), fluxo.getValCredICMS(), fluxo.getValDuplicata());
+				}
+			}
+		}
+		return lFluxoMes;
 	}
 
 	private boolean isDataVencimentoValida(Date dtVencimento) {
